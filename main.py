35700cfed9a474e8e029a4b1a421f34dcbe6543b
elabs <elijahknsubuga@gmail.com>
2022-02-16 20:12:55 +0300

    buttons

--- main.py
+++ main.py
@@ -15,6 +15,38 @@ class PomodoroTimer:
         self.s = ttk.Style()
         self.s.configure("TNoteBook.Tab", font=("Ubuntu", 16))
         self.s.configure("TButton", font=("Ubuntu", 16))
+        
+        self.tabs = ttk.Notebook(self.root)
+        self.tabs.pack(fill="both", pady=10, expand=True)
+        
+        self.tab1 = ttk.Frame(self.tabs, width=600, height=100)
+        self.tab2 = ttk.Frame(self.tabs, width=600, height=100)
+        self.tab3 = ttk.Frame(self.tabs, width=600, height=100)
+
+        self.pomodoro_timer_label = ttk.Label(self.tab1, text="25:00", font=("Ubuntu", 48))
+        self.pomodoro_timer_label.pack(pady=20)
+        
+        self.short_break_timer_label = ttk.Label(self.tab2, text="05:00", font=("Ubuntu", 48))
+        self.short_break_timer_label.pack(pady=20)
+        
+        self.long_break_timer_label = ttk.Label(self.tab3, text="15:00", font=("Ubuntu", 48))
+        self.long_break_timer_label.pack(pady=20)
+        
+        self.tabs.add(self.tab1, text="Pomodoro")
+        self.tabs.add(self.tab2, text="Short Break")
+        self.tabs.add(self.tab3, text="Long Break")
+        
+        self.grid_layout = ttk.Frame(self.root)
+        self.grid_layout.pack(pady=10)
+        
+        self.start_button = ttk.Button(self.grid_layout, text="Start", command=self.start_timer())
+        self.start_button.grid(row=0, column=0)
+        
+        self.start_button = ttk.Button(self.grid_layout, text="Skip", command=self.skip_clock())
+        self.start_button.grid(row=0, column=1)
+        
+        self.start_button = ttk.Button(self.grid_layout, text="Reset", command=self.skip_clock())
+        self.start_button.grid(row=0, column=2)
 
         self.root.mainloop()
     
