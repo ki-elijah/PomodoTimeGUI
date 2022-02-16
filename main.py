baefce7b65c060b2ad68975b4de8bd8cf321dc65
elabs <elijahknsubuga@gmail.com>
2022-02-16 13:23:47 +0300

    changed the icon

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class PomodoroTimer:
         self.root = tk.Tk()
         self.root.geometry("600x300")
         self.root.title("Elijah's Pomodoro Timer")
-        self.root.tk.call('wm', 'iconphoto', self.root._w, PhotoImage(file="UgandanFlag.png"))
+        self.root.tk.call('wm', 'iconphoto', self.root._w, PhotoImage(file="pomodoro.png"))
         
         self.s = ttk.Style()
         self.s.configure("TNoteBook.Tab", font=("Ubuntu", 16))
