9f206e99cd43b65500e4a049f22a63fbd06b4b6c
elabs <elijahknsubuga@gmail.com>
2022-02-16 13:17:58 +0300

    first

---
+++ main.py
@@ -0,0 +1,33 @@
+import time
+import threading
+import tkinter as tk
+from tkinter import ttk, PhotoImage
+
+class PomodoroTimer:
+    
+    def __init__(self):
+        
+        self.root = tk.Tk()
+        self.root.geometry("600x300")
+        self.root.title("Elijah's Pomodoro Timer")
+        self.root.tk.call('wm', 'iconphoto', self.root._w, PhotoImage(file="UgandanFlag.png"))
+        
+        self.s = ttk.Style()
+        self.s.configure("TNoteBook.Tab", font=("Ubuntu", 16))
+        self.s.configure("TButton", font=("Ubuntu", 16))
+
+        self.root.mainloop()
+    
+    def start_timer_thread(self):
+        pass
+    
+    def start_timer(self):
+        pass
+    
+    def reset_clock(self):
+        pass
+    
+    def skip_clock(self):
+        pass
+ 
+PomodoroTimer()
